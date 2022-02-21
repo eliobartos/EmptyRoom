79d659b2fa753b8841f0c04c7efba22a8f617bf8
FILIP BOSNIC <fbosnic@GIS-CL20-73522.ARES.shared-svc.local>
2022-02-21 11:28:46 +0100

    Helper for displaying generated worlds in command line

---
+++ world_generation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace world_generation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wg = new GameWorld(50, 50);
+            Int32 seed = (Int32) (new Random()).Next();
+            wg.generate_world(seed);
+            wg._place_rewards_on_stages();
+            for (int i = 0; i < wg.stages.Count; i++) {
+                Console.ReadLine();
+                Console.Clear();
+                GameWorld.print_world_matrix_to_console(wg.stages[i]);
+            }
+        }
+    }
+}
