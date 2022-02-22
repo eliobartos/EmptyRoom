a29afc878cf2ec28a54fd7b0e28ac778eb6447ed
Elio Bartos <eliobartos@yahoo.com>
2022-02-22 22:26:31 +0100

    added player animations

--- EmptyRoom/Assets/Scripts/PlayerMovement.cs
+++ EmptyRoom/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    public float sanityPctForPlayerSpriteChange = 0.2f;
     public float movementSpeed = 0.01f;
+    Animator animator;
+
+    public void Start() {
+        animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -21,6 +27,53 @@ public class PlayerMovement : MonoBehaviour
         pos.y = pos.y + moveVector.y * movementSpeed * Time.deltaTime;
 
         this.transform.position = pos;
+
+        HandleAnimations(inputX, inputY);
+    }
+
+    private void HandleAnimations(float inputX, float inputY) {
+
+        if(inputX == 0 & inputY == 0) {
+            animator.speed = 0;
+        } else {
+            animator.speed = 1;
+        }
+
+        if(GameManager.instance.ballsCollected == 0) {
+            if(inputX < 0) {
+                animator.Play("Left", 0);
+            } else if(inputX > 0) {
+                animator.Play("Right", 0);
+            } else if(inputY > 0) {
+                animator.Play("Up");
+            } else if(inputY < 0) {
+                animator.Play("Down");
+            }
+
+        } else if(GameManager.instance.ballsCollected > 0 & GameManager.instance.currentSanity/GameManager.instance.maxSanity > sanityPctForPlayerSpriteChange) {
+
+            if(inputX < 0) {
+                animator.Play("Left-Lantern", 0);
+            } else if(inputX > 0) {
+                animator.Play("Right-Lantern", 0);
+            } else if(inputY > 0) {
+                animator.Play("Up-Lantern");
+            } else if(inputY < 0) {
+                animator.Play("Down-Lantern");
+            }
+        } else {
+
+            if(inputX < 0) {
+                animator.Play("Left-Red", 0);
+            } else if(inputX > 0) {
+                animator.Play("Right-Red", 0);
+            } else if(inputY > 0) {
+                animator.Play("Up-Red");
+            } else if(inputY < 0) {
+                animator.Play("Down-Red");
+            }
+        }
+        
     }
 
     
